953d92ba0309e66be8e788fd69a830bd683a3254
Martin Kastner <mk@martinkastner.de>
2016-06-25 17:58:39 +0200

    added context to base path

--- config/config.js
+++ config/config.js
@@ -2,8 +2,9 @@ var config = {};
 
 // port on api listens
 config.port = 3002;
+config.context = 'colorshape';
 config.version = 'v1';
-config.base = '/' + config.version;
+config.base = '/' + config.context + '/' + config.version;
 
 config.name = 'colorshape.io backend';
 // access origin for CORS stuff
